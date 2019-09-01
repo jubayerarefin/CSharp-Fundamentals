e87cc3cd093ffe44b754a1fae74e1adbac24e34e
Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2019-09-01 23:43:30 +0600

    1st Commit
    Project is setup
    
    Created a sampel console app

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Output with a ternary
+            String output = args.Length > 0 ? $"Hello {args[0]}!" : $"Hello Stranger!";
+            Console.WriteLine(output);
+
+            //Print all parameters with a loop
+            if (args.Length > 0)
+            {
+                foreach (var arg in args)
+                {
+                    Console.WriteLine($"Hello {arg}!");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Hello Stranger!");
+            }
+        }
+    }
+}
