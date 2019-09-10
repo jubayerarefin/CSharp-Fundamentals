8490c7488cdc77f6fcf3ff7ff9d04382ae95420b
Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2019-09-10 19:00:27 +0600

    Added codes to list some floating point numbers calculate their sum and average

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            //Testing basic printing from cli args
+            printTest(args);
+            //Calculate Grade from a Double Type List
+            List<Double> Grades = new List<Double>() { 32.50, 46.50, 50.29, 49.99 };
+            calculateGrade(Grades);
+        }
+
+        //Printing variable length CLI args
+        public static void printTest(string[] args)
         {
             //Output with a ternary
             String output = args.Length > 0 ? $"Hello {args[0]}!" : $"Hello Stranger!";
@@ -22,6 +33,21 @@ namespace GradeBook
             {
                 Console.WriteLine($"Hello Stranger!");
             }
+        }
+
+        //Calculate Grade from a Double Type List
+        public static void calculateGrade(List<Double> Grades)
+        {
+            Double total = 0.0;
+            Double avg;
+            //Sum and Average grade
+            foreach (var marks in Grades)
+            {
+                total += marks;
+            }
+            avg = total / Grades.Count;
+            Console.WriteLine($"Total: {total}");
+            Console.WriteLine($"Result: {avg}");
         }
     }
 }
